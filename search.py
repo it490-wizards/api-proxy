d93712ff095229be12740a9c90284b41d47aba00
Andrew Kritzler <10812727+peppermintpatty5@users.noreply.github.com>
2021-10-27 16:42:56 +0000

    Testing the "Title" endpoint with key filter

--- search.py
+++ search.py
@@ -8,15 +8,40 @@ import requests
 IMDB_API_KEY = os.getenv("IMDB_API_KEY")
 
 
-def searchMovie(query: str):
+def searchMovie(query: str) -> list:
     response = requests.get(
         f"https://imdb-api.com/en/API/SearchMovie/{IMDB_API_KEY}/{query}"
     )
-    results = response.json()["results"]
+    result = response.json()
 
-    for result in results:
-        print(result)
+    return result["results"]
+
+
+def getTitle(imdb_id: str) -> dict:
+    # which data to extract from API result
+    keyFilter = [
+        "id",
+        "title",
+        "year",
+        "releaseDate",
+        "runtimeMins",
+        "image",
+        "genreList",
+    ]
+
+    response = requests.get(
+        f"https://imdb-api.com/en/API/Title/{IMDB_API_KEY}/{imdb_id}"
+    )
+    result = response.json()
+
+    return {key: result[key] for key in keyFilter}
 
 
 if __name__ == "__main__":
-    searchMovie(sys.argv[1])
+    if IMDB_API_KEY is None:
+        print("'IMDB_API_KEY' not set", file=sys.stderr)
+        exit(1)
+
+    # TODO: temporary
+    titles = searchMovie(sys.argv[1])
+    print(getTitle(titles[0]["id"]))
